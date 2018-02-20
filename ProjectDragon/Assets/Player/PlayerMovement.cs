7358819454cf99605090bbf5371ca32bacc106de
Kyle Bue <kylebue9@gmail.com>
2018-02-20 14:20:01 -0800

    Added support for multiple types of controllers

--- ProjectDragon/Assets/Player/PlayerMovement.cs
+++ ProjectDragon/Assets/Player/PlayerMovement.cs
@@ -11,7 +11,9 @@ public class PlayerMovement : MonoBehaviour
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool isInDirectMode = false; // TODO consider making static later
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,6 +23,35 @@ public class PlayerMovement : MonoBehaviour
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // G for gamepad. TODO add to menu or autoswitch
+        {
+            isInDirectMode = !isInDirectMode; // toggle mode
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
     {
         if (Input.GetMouseButton(0))
         {
